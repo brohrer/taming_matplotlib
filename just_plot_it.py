7ef09e1a6aa6ded6b0c711d8f3dee73037ac4f97
brohrer <brohrer@gmail.com>
2019-03-25 08:26:15 -0400

    images added

--- just_plot_it.py
+++ just_plot_it.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x_line = np.linspace(-2 * np.pi, 2 * np.pi, 500)
-y_line = np.sinc(x_line)
+x_curve = np.linspace(-2 * np.pi, 2 * np.pi, 500)
+y_curve = np.sinc(x_curve)
 
 plt.figure()
-plt.plot(x_line, y_line)
+plt.plot(x_curve, y_curve)
 plt.savefig("images/line_plot.png")
 plt.show()
 
