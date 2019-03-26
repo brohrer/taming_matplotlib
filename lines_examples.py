1b0e42a2841964a033be5e745e2a5c77deb2ad01
brohrer <brohrer@gmail.com>
2019-03-25 21:01:03 -0400

    images added

---
+++ lines_examples.py
@@ -0,0 +1,38 @@
+import matplotlib
+matplotlib.use("agg")
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Create the curve data.
+x = np.linspace(-6, 6, 500)
+y = np.sinc(x)
+
+# Vary the line width.
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(x, y, linewidth=5)
+fig.savefig("images/wide_line.png")
+
+# Vary the line color.
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(x, y, color="pink", linewidth=5)
+fig.savefig("images/pink_line.png")
+
+# Vary the line style.
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(x, y, linewidth=2, linestyle="--")
+fig.savefig("images/dashed_line.png")
+
+# Make multiple lines.
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(x, y, linewidth=2)
+ax.plot(x, y + .1, linewidth=1)
+ax.plot(x, y + .2, linewidth=.5)
+ax.plot(x, y + .3, linewidth=.2)
+ax.plot(x, y + .4, linewidth=.1)
+fig.savefig("images/multiple_lines.png")
