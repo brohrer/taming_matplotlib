d8b4117374a255a0e3c7265d66106aea2bb48b17
brohrer <brohrer@gmail.com>
2019-04-01 07:40:34 -0400

    images updated

--- text_examples.py
+++ text_examples.py
@@ -13,71 +13,91 @@ y = np.sinc(x)
 fig = plt.figure()
 ax = fig.gca()
 ax.plot(x, y)
-ax.set_xlabel("The x-axis label")
-ax.set_ylabel("The y-axis label")
-ax.set_title("The Title")
+ax.set_xlabel("The x-axis label", fontsize=16)
+ax.set_ylabel("The y-axis label", fontsize=16)
+ax.set_title("The Title", fontsize=18)
 fig.savefig("images/text_xy_labels_title.png")
 
 # Add some text.
 fig = plt.figure()
 ax = fig.gca()
 ax.plot(x, y)
-ax.text(-5, .8, "Text on a plot", fontsize=16)
+ax.text(-5, .8, "Text on a plot", fontsize=22)
 fig.savefig("images/text_any_text.png")
 
 # Align the text.
 fig = plt.figure()
 ax = fig.gca()
 ax.set_xlim([-2, 2])
-ax.set_ylim([-2, 2])
-ax.scatter(-1, -1, marker=".", c="black")
-ax.text(-1, -1, "right-top",
-        horizontalalignment="right",
-        verticalalignment="top")
-ax.scatter(1, -1, marker=".", c="black")
-ax.text(1, -1, "left-top",
-        horizontalalignment="left",
-        verticalalignment="top")
-ax.scatter(-1, 1, marker=".", c="black")
-ax.text(-1, 1, "right-bottom",
-        horizontalalignment="right",
-        verticalalignment="bottom")
-ax.scatter(1, 1, marker=".", c="black")
-ax.text(1, 1, "left-bottom",
+ax.set_ylim([-1.4, 1.4])
+left_pos = -1.8
+right_pos = 1.8
+top_pos = 1
+bottom_pos = -1
+center_pos = 0
+baseline_pos = -.5
+fontsize = 14
+ax.scatter(left_pos, bottom_pos, marker=".", c="black")
+ax.text(left_pos, bottom_pos, "left-bottom",
+        fontsize=fontsize,
         horizontalalignment="left",
         verticalalignment="bottom")
-ax.scatter(0, -1, marker=".", c="black")
-ax.text(0, -1, "horizcenter-top",
-        horizontalalignment="center",
+ax.scatter(right_pos, bottom_pos, marker=".", c="black")
+ax.text(right_pos, bottom_pos, "right-bottom",
+        fontsize=fontsize,
+        horizontalalignment="right",
+        verticalalignment="bottom")
+ax.scatter(left_pos, top_pos, marker=".", c="black")
+ax.text(left_pos, top_pos, "left-top",
+        fontsize=fontsize,
+        horizontalalignment="left",
         verticalalignment="top")
-ax.scatter(0, 1, marker=".", c="black")
-ax.text(0, 1, "horizcenter-bottom",
+ax.scatter(right_pos, top_pos, marker=".", c="black")
+ax.text(right_pos, top_pos, "right-top",
+        fontsize=fontsize,
+        horizontalalignment="right",
+        verticalalignment="top")
+ax.scatter(center_pos, bottom_pos, marker=".", c="black")
+ax.text(center_pos, bottom_pos, "center-bottom",
+        fontsize=fontsize,
         horizontalalignment="center",
         verticalalignment="bottom")
-ax.scatter(1, 0, marker=".", c="black")
-ax.text(1, 0, "left-vertcenter",
-        horizontalalignment="left",
-        verticalalignment="center")
-ax.scatter(-1, 0, marker=".", c="black")
-ax.text(-1, 0, "right-vertcenter",
+ax.scatter(center_pos, top_pos, marker=".", c="black")
+ax.text(center_pos, top_pos, "center-top",
+        fontsize=fontsize,
+        horizontalalignment="center",
+        verticalalignment="top")
+ax.scatter(right_pos, center_pos, marker=".", c="black")
+ax.text(right_pos, center_pos, "right-center",
+        fontsize=fontsize,
         horizontalalignment="right",
         verticalalignment="center")
-ax.scatter(1, .5, marker=".", c="black")
-ax.text(1, .5, "left-baseline",
+ax.scatter(left_pos, center_pos, marker=".", c="black")
+ax.text(left_pos, center_pos, "left-center",
+        fontsize=fontsize,
         horizontalalignment="left",
-        verticalalignment="baseline")
-ax.scatter(-1, .5, marker=".", c="black")
-ax.text(-1, .5, "right-baseline",
+        verticalalignment="center")
+ax.scatter(right_pos, baseline_pos, marker=".", c="black")
+ax.text(right_pos, baseline_pos, "right-baseline",
+        fontsize=fontsize,
         horizontalalignment="right",
         verticalalignment="baseline")
-ax.scatter(0, .5, marker=".", c="black")
-ax.text(0, .5, "center-baseline",
+ax.scatter(left_pos, baseline_pos, marker=".", c="black")
+ax.text(left_pos, baseline_pos, "left-baseline",
+        fontsize=fontsize,
+        horizontalalignment="left",
+        verticalalignment="baseline")
+ax.scatter(center_pos, baseline_pos, marker=".", c="black")
+ax.text(center_pos, baseline_pos, "center-baseline",
+        fontsize=fontsize,
         horizontalalignment="center",
         verticalalignment="baseline")
-ax.scatter(0, 0, marker=".", c="black")
-ax.text(0, 0, "center-center",
+ax.scatter(center_pos, center_pos, marker=".", c="black")
+ax.text(center_pos, center_pos, "center-center",
+        fontsize=fontsize,
         horizontalalignment="center",
         verticalalignment="center")
+ax.set_title("Text alignment options", fontsize=18)
 fig.savefig("images/text_alignment.png")
 
 # Change the text formatting. 
@@ -85,88 +105,108 @@ fig = plt.figure()
 ax = fig.gca()
 ax.set_xlim([-2, 2])
 ax.set_ylim([-2, 2])
-ax.text(-1, 1, 'fontsize=16', fontsize=16,
+fontsize = 16
+ax.text(-1, 1, 'fontsize=20', fontsize=20,
         horizontalalignment="center")
 ax.text(-1, 0, 'fontstyle="italic"', fontstyle="italic",
+        fontsize=fontsize,
         horizontalalignment="center")
 ax.text(-1, -1, 'fontweight="bold"', fontweight="bold",
+        fontsize=fontsize,
         horizontalalignment="center")
 ax.text(1, -1, "rotation=15", rotation=15,
+        fontsize=fontsize,
         horizontalalignment="center")
 ax.text(1, 0, 'color="red"', color="red",
+        fontsize=fontsize,
         horizontalalignment="center")
 ax.text(1, 1, 'family="serif"', family="serif",
+        fontsize=fontsize,
         horizontalalignment="center")
+ax.set_title("A sampling of text styling options", fontsize=18)
 fig.savefig("images/text_styles.png")
 
 # Text box
 fig = plt.figure()
 ax = fig.gca()
 ax.plot(x, y)
-ax.text(2, .4, "Text in a box",
+ax.text(1, .7, "Text in a box",
+        fontsize=20,
         bbox=dict(boxstyle = "square",
                   facecolor = "white"))
+ax.set_title("A bounding box", fontsize=18)
 fig.savefig("images/text_bbox.png")
 
 # Annotation
 fig = plt.figure()
 ax = fig.gca()
 ax.plot(x, y)
+fontsize = 20
 ax.annotate(
-    '"arrowstyle" = "-"',
+    '"-"',
     (2.4, np.sinc(2.4)),
     (3, .4),
     arrowprops=dict(arrowstyle = "-"),
+    fontsize=fontsize,
 )
 ax.annotate(
-    '"arrowstyle" = "-|>"',
+    '"-|>"',
     (-2.4, np.sinc(2.4)),
     (-5, .3),
     arrowprops=dict(arrowstyle = "-|>"),
+    fontsize=fontsize,
 )
 ax.annotate(
-    '"arrowstyle" = "<->"',
+    '"<->"',
     (.5, np.sinc(.5)),
     (3, .6),
     arrowprops=dict(arrowstyle = "<->"),
+    fontsize=fontsize,
 )
 ax.annotate(
-    '"arrowstyle" = "simple"',
+    '"simple"',
     (0, 1),
     (-6, .7),
     arrowprops=dict(arrowstyle = "simple"),
+    fontsize=fontsize,
 )
 ax.annotate(
-    '"arrowstyle" = "fancy"',
+    '"fancy"',
     (-.7, np.sinc(.7)),
     (-6, .55),
     arrowprops=dict(arrowstyle = "fancy"),
+    fontsize=fontsize,
 )
+ax.set_title("A few arrowstyle options", fontsize=18)
 fig.savefig("images/text_annotate.png")
 
 # LaTeX equations. 
-matplotlib.rcParams['text.usetex'] = True
 fig = plt.figure()
 ax = fig.gca()
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
-# ax.text(0, 0, r"$y = \Sum{x^2 dx}_{i=0}^{n}$",
+matplotlib.rcParams['text.usetex'] = True
 ax.text(
     0, 0,
     r"$f(x | \mu, \sigma^2) = \frac{1}{\sqrt{2\pi\sigma^2}}" +
     r"e^{- \frac{(x - \mu)^2}{2\sigma^2}}$",
     horizontalalignment="center",
     fontsize=24)
+matplotlib.rcParams['text.usetex'] = False
+ax.set_title("So much math", fontsize=18)
 fig.savefig("images/text_equation.png")
 
 fig = plt.figure()
 ax = fig.gca()
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
+matplotlib.rcParams['text.usetex'] = True
 ax.text(
     0, 0, r"$E=mc^2$",
     horizontalalignment="center",
-    fontsize=24)
+    fontsize=32)
+matplotlib.rcParams['text.usetex'] = False
+ax.set_title("Some math", fontsize=18)
 fig.savefig("images/text_equation_thumbnail.png")
 matplotlib.rcParams['text.usetex'] = False
 
